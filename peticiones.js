423c7d78eccbe9a23157b90bc5d8f110d0efa8a7
Nmonroy <61212361+Nmonroy93@users.noreply.github.com>
2020-07-21 12:31:20 -0600

    Update peticiones.js
    Puntos de verificación
    Crea varias rutas y por cada llamada desde el navegador debes imprimir una línea como la siguiente:
    
    <FECHA-HORA> <METODO> <URL> <IP> <NAVEGADOR>

--- peticiones.js
+++ peticiones.js
@@ -3,10 +3,11 @@ const app = express()
 const port = 8080
 const fecha_hoy= new Date();
 
-app.use((peticion,respuesta,siguiente)=>{console.log(fecha_hoy+" "+peticion.method);
+app.use((peticion,respuesta,siguiente)=>
+{console.log(fecha_hoy+" "+peticion.method+ " "+peticion.url+ " "+peticion.connection.remoteAddress+""+peticion.headers["user-agent"]);
     siguiente();
   })
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
 app.listen(port, () => console.log(`escuchando por el puerto!`))
 
